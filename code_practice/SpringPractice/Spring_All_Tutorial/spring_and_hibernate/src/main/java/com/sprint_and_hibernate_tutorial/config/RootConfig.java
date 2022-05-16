2fe57f66d6cefc3f7b63f384ec12d5a4f8da064c
Alamin <alamin8740@gmail.com>
2022-05-16 22:10:23 +0600

    hibernate-practice2

--- code_practice/SpringPractice/Spring_All_Tutorial/spring_and_hibernate/src/main/java/com/sprint_and_hibernate_tutorial/config/RootConfig.java
+++
@@ -1,8 +0,0 @@
-package com.sprint_and_hibernate_tutorial.config;
-
-import org.springframework.context.annotation.Import;
-
-@Import(DbConfig.class)
-public class RootConfig {
-
-}
--- code_practice/SpringPractice/Spring_All_Tutorial/spring_and_hibernate/src/main/webapp/WEB-INF/views/helloworld.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>Home work 1</title>
-</head>
-<body>
-<h1>Hello World running for spring and hibernate project</h1>
-</body>
-</html>

ab4ca1b68d21b8e90deced81cd135587939af55e
Alamin <alamin8740@gmail.com>
2022-05-28 23:03:11 +0600

    java practice change

--- code_practice/JavaPractice/JAVA/src/pttern/StarPrinting.java
+++ code_practice/JavaPractice/JAVA/src/pttern/StarPrinting.java
@@ -1,21 +1,22 @@
 package pttern;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StarPrinting {
     public static void main(String[] args) {
-        int row,col,n=5;
-        for(row = 1;row<=n;row++){
-            for(col=1;col<=row;col++){
-                System.out.print("p"+" ");
-            }
-            System.out.println();
-        }
+//       StarPrinting ob1 = new StarPrinting();
+//       Object obj2 = ob1;
+//        System.out.println(obj2 instanceof Object);
+//        System.out.println(obj2 instanceof StarPrinting);
 
-        for(row=n;row>=1;row--){  // here (n-1) because of same line not printing again
-            for(col=1;col<=row;col++){
-                System.out.print("p"+" ");
-            }
-            System.out.println();
-        }
+//        Map map = new HashMap();
+//        map.put("java",1);
+//        map.put("therap",2);
+//        Integer val = 4;
+//        map.put("java",val);
+//        val = val + 5;
+//        System.out.println(val + " " + map.get("java"));
 
     }
 }
